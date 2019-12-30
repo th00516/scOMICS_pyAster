85d0d7944a4966e5e43334e68f2ef1049f801df9
yuhao <yuhao819@hotmail.com>
2019-12-30 22:41:32 +0800

    The test version of Hamming distance of Site-kmer strategy

--- utils/refFree_test.kmer_hamming.py
+++ utils/refFree_test.kmer_hamming.py
@@ -9,3 +9,33 @@ import numpy as np
 from itertools import product
 
 
+class kmer_encoder:
+    def __init__(self):
+        """"""
+        self.read_seq_mat = []
+        self.read_seq_len = 0
+
+        self.kmer_win = np.array([], dtype=np.float32)
+
+    def file_buffer(self, read_file):
+        """"""
+        with gzip.open(read_file, 'rt') as FIN:
+            for _ in FIN:
+                line = FIN.readline().strip()
+
+                self.read_seq_len = len(line)
+                line = line[5:self.read_seq_len - 5]
+                self.read_seq_len = len(line)
+
+                self.read_seq_mat.append(line)
+
+                FIN.readline()
+                FIN.readline()
+
+    def kmer_hamming(self):
+        """"""
+        pass
+
+
+if __name__ == '__main__':
+    pass
