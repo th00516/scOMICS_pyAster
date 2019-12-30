dc1bb812c37262c8ed2e680a2c9c4301da1e7a52
yuhao <yuhao819@hotmail.com>
2019-12-30 22:47:05 +0800

    The test version of Site-kmer strategy

--- utils/refFree_encode.site_kmer.py
+++ utils/refFree_encode.site_kmer.py
@@ -32,7 +32,7 @@ class kmer_encoder:
                 FIN.readline()
                 FIN.readline()
 
-    def encode_kmer(self, K):
+    def encode_site_kmer(self, K):
         """"""
         total_kmer_freq = []
 
@@ -53,14 +53,31 @@ class kmer_encoder:
 
         self.kmer_win = np.asarray(total_kmer_freq)
 
-    def encode_kmer_hamming(self, K):
+    def encode_kmer(self, K):
         """"""
-        pass
+        total_kmer_freq = []
+
+        kmer_freqs = {''.join(_): 0 for _ in product(('A', 'T', 'C', 'G'), repeat=K)}
+
+        for i in range(0, self.read_seq_len - (K - 1)):
+            kmer_counts = dict(pd.value_counts([_[i:i + K] for _ in self.read_seq_mat], sort=False))
+
+            total_kmer_count = sum(kmer_counts.values())
+
+            for kmer_seq in kmer_freqs.keys():
+                if kmer_seq in kmer_counts:
+                    kmer_freqs[kmer_seq] = kmer_counts[kmer_seq] / total_kmer_count
+
+            kmer = [kmer_freqs[_] for _ in sorted(kmer_freqs.keys())]
+
+            total_kmer_freq.append(kmer)
+
+        self.kmer_win = np.asarray(total_kmer_freq)
 
 
 if __name__ == '__main__':
     encoder = kmer_encoder()
     encoder.file_buffer(sys.argv[1])
-    encoder.encode_kmer(6)
+    encoder.encode_site_kmer(6)
 
     np.save(sys.argv[1] + '.npy', np.around(encoder.kmer_win / np.max(encoder.kmer_win) * 255))
