b3ba3310eb5cb98facc82d42a18ed7b8817f30a9
yuhao <yuhao819@hotmail.com>
2020-01-09 03:47:56 +0800

    Add function for assessing frequency of k-mer

--- utils/kmer_types_collection.py
+++ utils/kmer_types_collection.py
@@ -13,9 +13,10 @@ class kmer:
         self.seq_mat = []
         self.seq_len = 0
         self.kmer_set = {}
+        self.kmer_motif_type_set = {}
         self.kmer_motif_set = {}
 
-        self.k_typ = {'A': 1, 'C': 2, 'G': 4, 'T': 8}
+        self.k_type_maker = {'A': 1, 'C': 2, 'G': 4, 'T': 8}
 
     def reshape_reads(self, read_file):
         """"""
@@ -59,16 +60,22 @@ class kmer:
 
         for k_seq in self.kmer_set.keys():
             if F_min <= self.kmer_set[k_seq] <= F_max:
-                k_seq = [_ for _ in k_seq]
-                k_sco = self.k_typ[k_seq[int(K / 2)]]
-                k_seq[int(K / 2)] = 'N'
-                k_seq = ''.join(k_seq)
+                k_seq_arr = [_ for _ in k_seq]
 
-                if k_seq not in self.kmer_motif_set:
-                    self.kmer_motif_set.update({k_seq: k_sco})
+                k_seqL = ''.join(k_seq_arr[:int(int(sys.argv[-1]) / 2)])
+                k_seqR = ''.join(k_seq_arr[int(int(sys.argv[-1]) / 2) + 1:])
+
+                k_seq_motif = ''.join((k_seqL, 'N', k_seqR))
+
+                k_mak = self.k_type_maker[k_seq_arr[int(K / 2)]]
+
+                if k_seq_motif not in self.kmer_motif_type_set:
+                    self.kmer_motif_type_set.update({k_seq_motif: k_mak})
+                    self.kmer_motif_set.update({k_seq_motif: self.kmer_set[k_seq]})
 
                 else:
-                    self.kmer_motif_set[k_seq] += k_sco
+                    self.kmer_motif_type_set[k_seq_motif] += k_mak
+                    self.kmer_motif_set[k_seq_motif] += self.kmer_set[k_seq]
 
 
 if __name__ == '__main__':
@@ -76,52 +83,36 @@ if __name__ == '__main__':
     k.reshape_reads(sys.argv[1])
     k.kmer_motif_stat(int(sys.argv[-1]))
 
-    # STAT = open(sys.argv[1] + '.snp_idx.stat', 'wt')
+    T_OU = open(sys.argv[1] + '.snp_idx.type_stat', 'wt')
+    F_OU = open(sys.argv[1] + '.snp_idx.freq_stat', 'wt')
 
-    with open(sys.argv[1] + '.snp_idx.lst', 'wt') as OU:
-        for i in sorted(k.kmer_motif_set.keys()):
-            x = k.kmer_motif_set[i]
+    for i in sorted(k.kmer_motif_type_set.keys()):
+        x = k.kmer_motif_type_set[i]
 
-            ###############################
-            # count number of 1 in binary #
-            ###############################
-            count = 0
-            while x:
-                x = x & (x - 1)
-                count += 1
-            ###############################
+        ###############################
+        # count number of 1 in binary #
+        ###############################
+        count = 0
+        while x:
+            x = x & (x - 1)
+            count += 1
+        ###############################
 
-            ####################################################
-            # only homozygous & dualistic SNP should be stored #
-            ####################################################
-            if count <= 2:
-                # code = '{:04b}'.format(k.kmer_motif_set[i])
+        ####################################################
+        # only homozygous & dualistic SNP should be stored #
+        ####################################################
+        if count <= 2:
+            i = [_ for _ in i]
 
-                i = [_ for _ in i]
+            iL = ''.join(i[:int(int(sys.argv[-1]) / 2)])
+            iR = ''.join(i[int(int(sys.argv[-1]) / 2) + 1:])
 
-                iL = ''.join(i[:int(int(sys.argv[-1]) / 2)])
-                iR = ''.join(i[int(int(sys.argv[-1]) / 2) + 1:])
+            score_t = k.kmer_motif_type_set[''.join((iL, 'N', iR))]
+            score_f = k.kmer_motif_set[''.join((iL, 'N', iR))]
 
-                # i0 = ''.join((iL, 'A', iR))
-                # i1 = ''.join((iL, 'C', iR))
-                # i2 = ''.join((iL, 'G', iR))
-                # i3 = ''.join((iL, 'T', iR))
-                #
-                # if int(code[3]) == 1:
-                #     print(i0 + ',' + os.path.basename(sys.argv[1]) + ',' + code[3], file=OU)
-                #
-                # if int(code[2]) == 1:
-                #     print(i1 + ',' + os.path.basename(sys.argv[1]) + ',' + code[2], file=OU)
-                #
-                # if int(code[1]) == 1:
-                #     print(i2 + ',' + os.path.basename(sys.argv[1]) + ',' + code[1], file=OU)
-                #
-                # if int(code[0]) == 1:
-                #     print(i3 + ',' + os.path.basename(sys.argv[1]) + ',' + code[0], file=OU)
+            print(''.join((iL, 'N', iR)) + ',' + os.path.basename(sys.argv[1]) + ',' + str(score_t), file=T_OU)
+            print(''.join((iL, 'N', iR)) + ',' + os.path.basename(sys.argv[1]) + ',' + str(score_f), file=F_OU)
+        ####################################################
 
-                print(''.join((iL, 'N', iR)) + ',' +
-                      os.path.basename(sys.argv[1]) + ',' +
-                      str(k.kmer_motif_set[''.join((iL, 'N', iR))]), file=OU)
-            ####################################################
-
-    # STAT.close()
+    T_OU.close()
+    F_OU.close()
